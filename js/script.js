78aba2954ea6fd6e6b82f8af5ae9aa7b88ac420a
STEVE KORNAHRENS <steve.kornahrens@gmail.com>
2017-08-06 14:31:12 -0600

    save to experiment with multi get requests

--- js/script.js
+++ js/script.js
@@ -33,9 +33,6 @@ $('#RemoveGroup').click(function () {
 jQuery.fn.pop = [].pop;
 jQuery.fn.shift = [].shift;
 
-var GroupData = [];
-var GroupURLs = [];
-
 $BTN.click(function () {
   var $rows = $TABLE.find('tr:not(:hidden)');
   var headers = [];
@@ -51,7 +48,7 @@ $BTN.click(function () {
     var $td = $(this).find('td');
     var h = {};
 
-    // Use the headers from earlier to name our hash keys
+    // Grabs hidden header (group) to create an object key
     headers.forEach(function (header, i) {
       h[header] = $td.eq(i).text();
     });
@@ -70,121 +67,109 @@ $BTN.click(function () {
 
 // Meetup.com API Key for Steve Kornahrens
 var MeetupKey = "6a3426d1c7d3565234713b22683948";
+
+//Variables needed for Data Gathering and Looping
 var EventData = [];
+var GroupData = [];
+var GroupURLs = [];
+//var count is used to create unique IDs for each event appended to #ListAggro
+var count = 1;
 var PassedUrl;
 
 //Gets Group Event Data for Each Group Given
-$("#GetData").click(function(event){
-  console.log("-----")
-  console.log("GroupURLs:" + GroupURLs)
-  console.log(GroupURLs)
+$("#GetData").click(function(){
   for (var i = 0; i < GroupURLs.length; i++) {
+    //PassedUrl = Group Name represented by an index of GroupURLs
     PassedUrl = GroupURLs[i];
-    console.log("-----")
-    console.log("PassedUrl: ");
-    console.log(PassedUrl)
     $.ajax({
       method: "GET",
       url: "https://galvanize-cors-proxy.herokuapp.com/https://api.meetup.com/2/events?key=" + MeetupKey + "&group_urlname=" + PassedUrl + "&sign=true"
     })
     .then(function (data) {
+      EventData = [];
       EventData.push(data.results);
+      var $EVENT = $('#LinkAggro');
+      for (var j = 0; j < EventData.length; j++) {
+        var EventObject = EventData[j];
 
-    //end of .then(function (data)
+        for (var x = 0; x < EventObject.length; x++) {
+          var $clone = $EVENT.find('div.hide').clone(true).removeClass('hide').attr('id', 'Event' + count);
+          $EVENT.find('.EventList').append($clone);
+
+          //Date Creation before assigning to EventLocation and EventTime
+          var date = new Date(EventObject[x].time)
+          var month = date.getUTCMonth() + 1;
+
+          //Converts .getUTCDay into an actual day of the week to create var newdate
+          var weekday = new Array();
+          weekday[0] = "Sunday";
+          weekday[1] = "Monday";
+          weekday[2] = "Tuesday";
+          weekday[3] = "Wednesday";
+          weekday[4] = "Thursday";
+          weekday[5] = "Friday";
+          weekday[6] = "Saturday";
+          var dayofweek = weekday[date.getUTCDay()];
+          var day = date.getUTCDate();
+          var year = date.getUTCFullYear();
+          var newdate = dayofweek + " " + month + "/" + day + "/" + year;
+
+          //Converts given time into AM PM time to create var time
+          var d = new Date(EventObject[x].time);
+          var hh = d.getHours();
+          var m = d.getMinutes();
+          var dd = "AM";
+          var h = hh;
+          if (h >= 12) {
+            h = hh-12;
+            dd = "PM";
+          }
+          if (h == 0) {
+            h = 12;
+          }
+          m = m<10?"0"+m:m;
+          h = h<10?+h:h;
+          var time = h+":"+m+" "+dd;
+          //Event Date and Time
+          $("#Event" + count + " .EventTimeDate").text(newdate + " " + time);
+          //Event Group, Event Name, and Event ID
+          $("#Event" + count + " .GroupName").text(EventObject[x].group.name);
+          $("#Event" + count + " .EventName").text(EventObject[x].name);
+          $("#Event" + count + " .EventID").text(EventObject[x].id);
+
+          //if statement to check if the EventData Object contains a venue key
+          if (!EventObject[x].venue) {
+            $("#Event" + count + " .EventLocation").text("Location Not Selected");
+          } else {
+            $("#Event" + count + " .EventLocation").text(EventObject[x].venue.name);
+          }
+          //count iterates so loops to create a new Event id for each Event
+          count++;
+        }
+        //Create Event List - for loop
+      }
+      //end of .then(function (data)
     })
 
-
-  //end of get data for loop
+    //end of get data for loop
   };
-  console.log("-----");
-  console.log("EventData: ");
-  console.log(EventData);
-  // CreateEventList()
-//End of GetData Click Event
+  //end of onclick event at #GetData
 });
 
+//Event Listener for highlighting tables
+var SelectedEvents = [];
+$(".Event").click(function(){
+  $(this).toggleClass("teal EventSelected");
+});
 
-// function CreateEventList() {
+//Create array containing Event Information
+$("#GetSelectedEvents").click(function() {
+  $(".EventSelected").each(function (index){
+    console.log(index + ": " + $(this).text());
+    SelectedEvents.push(index + ": " + $(this).text());
 
-
-$("#CreateList").click(function(event){
-// Create Event List
-// Loops through each EventData index grabbing necessary details about group events
-// Creates one new .Event div per EventData index
-var $EVENT = $('#LinkAggro');
-var count = 1;
-console.log("Event Data log inside CreateEventList")
-console.log(EventData)
-console.log(EventData.length)
-
-
-for (var j = 0; j < EventData.length; j++) {
-  var EventObject = EventData[j];
-  console.log("-----")
-  console.log("EventObject: ")
-  console.log(EventObject)
+  })
 
 
-  for (var x = 0; x < EventObject.length; x++) {
-    // console.log("Event " + count)
-    // console.log(EventObject[i].name)
-    // console.log(new Date(EventObject[i].time))
-    // console.log(EventObject[i].group.name)
-    // console.log(EventObject[i].venue.name)
-    // console.log(EventObject[i].event_url)
-    // console.log("------")
-    var $clone = $EVENT.find('div.hide').clone(true).removeClass('hide').attr('id', 'Event' + count);
-    $EVENT.find('.EventList').append($clone);
-    //Date Creation before assigning to EventLocation and EventTime
-    var date = new Date(EventObject[x].time)
-    var month = date.getUTCMonth() + 1; //months from 1-12
 
-    //Converts .getUTCDay into an actual day of the week to create var newdate
-    var weekday = new Array();
-    weekday[0] = "Sunday";
-    weekday[1] = "Monday";
-    weekday[2] = "Tuesday";
-    weekday[3] = "Wednesday";
-    weekday[4] = "Thursday";
-    weekday[5] = "Friday";
-    weekday[6] = "Saturday";
-    var dayofweek = weekday[date.getUTCDay()];
-    var day = date.getUTCDate();
-    var year = date.getUTCFullYear();
-    var newdate = dayofweek + " " + month + "/" + day + "/" + year;
-
-    //Converts given time into AM PM time to create var time
-    var d = new Date(EventObject[x].time);
-    var hh = d.getHours();
-    var m = d.getMinutes();
-    var dd = "AM";
-    var h = hh;
-    if (h >= 12) {
-      h = hh-12;
-      dd = "PM";
-    }
-    if (h == 0) {
-      h = 12;
-    }
-    m = m<10?"0"+m:m;
-    h = h<10?+h:h;
-    var time = h+":"+m+" "+dd
-
-    $("#Event" + count + " .GroupName").text(EventObject[x].group.name);
-    $("#Event" + count + " .EventName").text(EventObject[x].name);
-
-    //if statement to check if the EventData Object contains a venue key
-    if (!EventObject[x].venue) {
-      $("#Event" + count + " .EventLocation").text("Location Not Selected");
-    } else {
-      $("#Event" + count + " .EventLocation").text(EventObject[x].venue.name);
-    }
-
-    $("#Event" + count + " .EventDate").text(newdate);
-    $("#Event" + count + " .EventTime").text(time);
-    count++;
-  }
-  //Create Event List - for loop
-}
-//.then CreateEventList() ends
 });
